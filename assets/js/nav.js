5b24698180789097eb3b97bf9cd86582705ccffd
John Lai <lai.john.py@gmail.com>
2023-01-17 22:11:57 -0500

    Add js selector and data attr to main nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,1 +1,7 @@
-console.log('test');
+const primaryNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute('data-visible');
+  console.log(visibility);
+});
